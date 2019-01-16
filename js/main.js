fe8e77ed148c20a0c50cc7753b673a0969be3264
Łukasz Złotorzyński <l.zlotorzynski@gmail.com>
2019-01-16 11:39:57 +0100

    add jquery scroll

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('a').on('click', function () {
+    const goToSection = "#" + $(this).attr('class');
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    }, 500)
+})
